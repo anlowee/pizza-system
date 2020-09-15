4b4f4a31c5663b2cb57cb608da3ba03a2524293a
Eddy Wei <anlowee@outlook.com>
2020-09-15 19:48:11 +0800

    :construction: :add rest api for america pizza store

--- src/main/java/com/example/pizzasystem/control/PizzaOrderController.java
+++ src/main/java/com/example/pizzasystem/control/PizzaOrderController.java
@@ -3,15 +3,14 @@ package com.example.pizzasystem.control;
 import com.example.pizzasystem.service.order.BaseOrder;
 import com.example.pizzasystem.service.order.OrderHandler;
 import com.example.pizzasystem.service.order.order.AmericaStylePizzaStoreOrder;
+import com.example.pizzasystem.service.order.order.BeverageOrder;
+import com.example.pizzasystem.service.order.order.ItalyStylePizzaStoreOrder;
 import com.example.pizzasystem.service.order.order.Order;
+import lombok.Data;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.AbstractList;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -21,13 +20,33 @@ import java.util.List;
 @RestController
 public class PizzaOrderController {
 
-    @GetMapping("/api/america/pizza")
+    @GetMapping("/api/america/order")
     public String orderPizzasAtAmericaStyleStore(
-            @RequestBody List<BaseOrder> pizzaOrders ) {
-        Order order = new AmericaStylePizzaStoreOrder(pizzaOrders);
+            @RequestBody RequestBean requestBean) {
+        Order pizzaOrdersIssue = new AmericaStylePizzaStoreOrder(requestBean.getPizzas());
+        Order beverageOrdersIssue = new BeverageOrder(requestBean.getBeverages());
         OrderHandler orderHandler = new OrderHandler();
-        orderHandler.setPizzaOrder(order);
+        orderHandler.setPizzaOrder(pizzaOrdersIssue);
+        orderHandler.setBeverageOrder(beverageOrdersIssue);
         return orderHandler.handleOrder();
     }
 
+    @GetMapping("/api/italy/order")
+    public String orderPizzasAtItalyStyleStore(
+           @RequestBody RequestBean requestBean
+    ) {
+        Order pizzaOrdersIssue = new ItalyStylePizzaStoreOrder(requestBean.getPizzas());
+        Order beverageOrdersIssue = new BeverageOrder(requestBean.getBeverages());
+        OrderHandler orderHandler = new OrderHandler();
+        orderHandler.setPizzaOrder(pizzaOrdersIssue);
+        orderHandler.setBeverageOrder(beverageOrdersIssue);
+        return orderHandler.handleOrder();
+    }
+
+}
+
+@Data
+class RequestBean {
+    private List<BaseOrder> pizzas;
+    private List<BaseOrder> beverages;
 }
